11a2344e66736750363c86e7cf72f5bb271e671f
Simon Godoy <sgodoy2017@gmail.com>
2026-08-04 19:47:12 -0500

    feat: update http port on core module

--- packages/core/src/ports/http.port.ts
+++ packages/core/src/ports/http.port.ts
@@ -1,7 +1,3 @@
-import { GenericPayload, Method } from '@silkon/common';
-
 export interface HttpPort {
-  invoke(method: Method, data: unknown, options: unknown): Promise<GenericPayload>;
-
-  invoke(url: string, method: Method, data: unknown, options: unknown): Promise<GenericPayload>;
+  invoke(request: unknown): Promise<unknown>;
 }
